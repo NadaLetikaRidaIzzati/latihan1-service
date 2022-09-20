a4c50ca3cf3e3cbc05ed97294fb7994d6a43f73b
NadaLetikaRidaIzzati <113491775+NadaLetikaRidaIzzati@users.noreply.github.com>
2022-09-20 15:00:30 +0700

    Add files via upload
    jdk-18.0.2.1
    Sping Boot-2.7.3
    Packaging jar

---
+++ Latihan-service/Latihan-service/src/test/java/com/Nada/Latihan/service/LatihanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nada.Latihan.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
